588987f1785a1af8c57f836d41714ec637988f01
ManasaTipparam <manasa.tipparam@target.com>
2020-02-21 14:33:28 -0600

    add main text to page

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,10 @@ export default function Index() {
   return (
     <div className={styles.root}>
       <Navbar/>
-      
+      <div className={styles.mainTextContainer}>
+        <p className={styles.helloText}>Hi, I'm Manasa.</p>
+        <p className={styles.helloSubText}>I'm a UI/UX enthusiast and Engineer based in Minneapolis.</p>
+      </div>
     </div>
   )
 }
